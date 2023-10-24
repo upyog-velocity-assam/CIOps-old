23c6cb616d693544675aab84f5e4155c61992590
kumarnarendra701 <kumarnarendra701@gmail.com>
2023-10-24 11:09:03 +0530

    update repo name

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -98,7 +98,7 @@ spec:
         node(POD_LABEL) {
 
             def scmVars = checkout scm
-            String REPO_NAME = env.REPO_NAME ? env.REPO_NAME : "docker.io/egovio";         
+            String REPO_NAME = env.REPO_NAME ? env.REPO_NAME : "docker.io/velsof";         
             String GCR_REPO_NAME = "asia.gcr.io/digit-egov";
             def yaml = readYaml file: pipelineParams.configFile;
             List<JobConfig> jobConfigs = ConfigParser.parseConfig(yaml, env);
